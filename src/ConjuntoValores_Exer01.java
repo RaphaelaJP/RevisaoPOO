14c50176964e98d2a7d16e03f923ce2ed6277140
RaphaelaJP <47481822+RaphaelaJP@users.noreply.github.com>
2021-01-14 17:31:53 -0300

    Lista de revisão de POO
    Lista de revisão de POO da disciplina de estrutura de dados I

--- src/ConjuntoValores_Exer01.java
+++ src/ConjuntoValores_Exer01.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class ConjuntoValores_Exer01 {
-    
+
+    private int conjuntoValores[] = new int [10];
+
+    Scanner scan = new Scanner(System.in);
+
+    public ConjuntoValores_Exer01(int[] conjuntoValores) {
+
+    }
+
+    public void informarValor (){
+        for (int i = 0; i < conjuntoValores.length; i++){
+            System.out.println("Informe o valor: ");
+            conjuntoValores[i] = scan.nextInt();
+        }
+
+    }
+
+    public void MostrarVetor(){
+        for (int i = 0; i < conjuntoValores.length; i++){
+            System.out.println("O valor do vetor é: " + conjuntoValores[i]);
+
+        }
+
+    }
+
+    public int[] getConjuntoValores() {
+        return conjuntoValores;
+    }
+
+    public void setConjuntoValores(int[] conjuntoValores) {
+        this.conjuntoValores = conjuntoValores;
+    }
 }
+
+
+
+
+
